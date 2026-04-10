fb5a9801b805daeb41f5228e1df27b02b6cc77c9
Hugues henri Delpit <hugueshenridelpit@MacBookAir.lan>
2026-04-10 17:47:23 +0200

    feat(presence): upgrade sheets scope to read/write for presence toggle

--- lib/google.ts
+++ lib/google.ts
@@ -10,6 +10,7 @@ const SCOPES = [
   'https://www.googleapis.com/auth/drive.readonly',
   'https://www.googleapis.com/auth/drive.file',
   'https://www.googleapis.com/auth/gmail.send',
+  'https://www.googleapis.com/auth/spreadsheets',
 ]
 
 /** Tokens Google persistés dans le cookie httpOnly `google_tokens`. */
